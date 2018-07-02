a41d86e8188e34ea006f413ccac490580eeda132
zhangzw218 <745787360@qq.com>
2018-07-03 00:28:27 +0800

    添加项目文件。

---
+++ GitWebZ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitWebZ.Startup))]
+namespace GitWebZ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
